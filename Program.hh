18989b9095cdc0f3ca49e0c44d20dd1d9f264321
chriswillis <chriswillis10@gmail.com>
2015-12-06 19:33:22 +0000

    Push

---
+++ Program.hh
@@ -0,0 +1,24 @@
+#ifndef Program_hh
+#define Program_hh
+
+#include <iostream>
+#include <vector>
+
+
+class Program{
+private:
+    std::vector<std::string> commands;
+
+public:
+	//Default constructor
+	Program();
+	//Destructor
+	~Program();
+
+	//Executes each command in turn
+	void run();
+
+    friend std::istream& operator>> (std::istream& in, Program& p);
+};
+
+#endif
