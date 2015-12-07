8ddf9ea12af91869df52f78ddde8c91cf332cbef
fjdurham94 <jackdurham@jacks-mbp>
2015-12-07 23:36:18 +0000

    Main written to read a file into P then draw it

--- main.cc
+++ main.cc
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 #include "window.h"
 #include "Translate.hh"
 #include "Rotate.hh"
+#include "Program.hh"
+
+using namespace std;
+
+Program P;
+
 int main(int argc, char** argv)
 {
     window w(argc, argv);
+    string fname = argv[1], output;
+    cout << "Reading file [" << fname << "]" << endl;
+    ifstream file(fname);
     
+    if (!file){
+        cout << "Don't think that file exists mate..." << endl;
+        return 1;
+    }
+    
+    file >> P;
+
     return 0;
 }
 
 void draw()
 {
-    for(int i=0; i<10; i++)
-    {
-        Translate t(1, true);
-        Rotate r(1, true);
-        
-    }
-
-    
+    P.run();
 }
