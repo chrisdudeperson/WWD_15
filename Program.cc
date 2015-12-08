8056ab937ce8084ec46199c2b46614c6f1d365c0
fjdurham94 <jazzy.jack@ntlworld.com>
2015-12-08 16:34:42 +0000

    Chris is a twat
    It now works so just needs re factoring and breaking

--- Program.cc
+++ Program.cc
@@ -31,49 +31,61 @@ void Program::run(){
 
 istream& operator>> (istream& in, Program& p){
 	string s;
-	int arg;
+    double arg;
     Command* p_cmd;
 
-	while (!in.eof()) {
+    while (!in.eof() && s != "]") {
 		in >> s;
-		in >> arg;
-		if (!in.fail()){
-            if (s == "FORWARD"){
-                cout<< "create a forward command with argument [" << arg << "]" << endl;
-                p_cmd = new Translate(arg, true);
-            } else if (s == "JUMP"){
-                cout<< "create a jump command with argument [" << arg << "]" << endl;
-                p_cmd = new Translate(arg, false);
-			} else if (s == "LEFT"){
-				cout<< "create a left command with argument [" << arg << "]" << endl;
-                p_cmd = new Rotate(arg, true);
-                //p_cmd = &cmd;
-			} else if (s == "RIGHT"){
-                cout<< "create a right command with argument [" << arg << "]" << endl;
-                p_cmd = new Rotate(arg, false);
-			} else if (s == "REPEAT"){
-				cout<< "create a repeat command with argument [" << arg << "]" << endl;
-			} else {
-				cout << "Unrecognised command [" << s << "]" << endl;
-				if (!in.eof()){
-					//There is something wrong with the file
-					if (in.fail()){
-						//Read failed due to bad file
-						throw BadFileException();
-					} else {
-						//Invalid command in file
-						throw BadCommandException();
-					}
-				} else {
-					cout << "EOF Reached" << endl;
-				}
-			}
-            //Slice the command back to the base object and push to the vector
-            p.commands.push_back(p_cmd);
-		} else {
-			if (in.eof())
-				cout << "Finished reading file!" << endl;
-		}
+        cout << "s: " << s << endl;
+        if (s != "]"){
+            in >> arg;
+            if (!in.fail()){
+                if (s == "FORWARD"){
+                    cout<< "create a forward command with argument [" << arg << "]" << endl;
+                    p_cmd = new Translate(arg, true);
+                } else if (s == "JUMP"){
+                    cout<< "create a jump command with argument [" << arg << "]" << endl;
+                    p_cmd = new Translate(arg, false);
+                } else if (s == "LEFT"){
+                    cout<< "create a left command with argument [" << arg << "]" << endl;
+                    p_cmd = new Rotate(arg, true);
+                } else if (s == "RIGHT"){
+                    cout<< "create a right command with argument [" << arg << "]" << endl;
+                    p_cmd = new Rotate(arg, false);
+                } else if (s == "REPEAT"){
+                    cout<< "create a repeat command with argument [" << arg << "]" << endl;
+                    Program re_cmds;
+                    char b;
+                    in>>ws>>b>>ws;
+                    if (b != '['){
+                        //Invalid repeat command
+                        throw BadCommandException();
+                    }
+                    in >> re_cmds;
+                    p_cmd = new Repeat(arg, re_cmds);
+                } else {
+                    cout << "Unrecognised command [" << s << "]" << endl;
+                    if (!in.eof()){
+                        //There is something wrong with the file
+                        if (in.fail()){
+                            //Read failed due to bad file
+                            throw BadFileException();
+                        } else {
+                            //Invalid command in file
+                            throw BadCommandException();
+                        }
+                    } else {
+                        cout << "EOF Reached" << endl;
+                    }
+                }
+                //Slice the command back to the base object and push to the vector
+                p.commands.push_back(p_cmd);
+            } else {
+                if (in.eof())
+                    cout << "Finished reading file!" << endl;
+            }
+        }
+        //cout << "Peek: " << in.peek() << endl;
 	}
 	return in;
 }
