502f1d2810a14fb87c42c741522e00be4f13ad92
willwestwood <william@ctech.co.uk>
2015-12-12 15:28:32 +0000

    Commenty

--- main.cc
+++ main.cc
@@ -13,37 +13,39 @@ Program P;
 
 int main(int argc, char** argv)
 {
+	// Read in argument and open file
 	char* fname = argv[1];
-	
 	ifstream file(fname);
 
+	// If file could not open, print error and exit
 	if (!file)
 	{
 		cout << "File not found: " << fname << endl;
 		return 1;
-	}
+	} 
 	else
 	{
 		cout << "Reading file [" << fname << "]" << endl << endl;
 	}
 
+	// Read file contents into program, and catch errors in file
 	try {
 		file >> P;
-	}
-	catch (BadFileException e) {
+	} catch (BadFileException e) {
 		cout << endl << "Bad file" << endl << endl;
 		return 1;
-	}
-	catch (BadCommandException e) {
+	} catch (BadCommandException e) {
 		cout << endl << "Bad command" << endl << endl;
 		return 1;
 	}
 
+	// Executes window
 	window w(argc, argv);
 
 	return 0;
 }
 
+// Runs list of commands
 void draw()
 {
 	cout << endl << "Drawing..." << endl;
